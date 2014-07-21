1d66c6e5dba5862b20c86378db91454c2ac803bb
Aaron Ellis Snyder <snyder@mojotech.com>
2014-07-21 14:15:10 -0400

    enable conversion of private/public goals
    private -> public should not create a new record, it should
    either use a public goal with the same title, or update this goal
    to become public
    
    public -> private should always create a new record, this
    ensures we don't lock other users out of a habit. theoretically
    we could convert the record instead of creating a new one if there
    are no other users on the goal
    
    New private goals should check for a private goal of the same
    title, use that if it exists.
    
    New public goals should check if a public goal of same title exists
    
    todo: public -> private doesn't have to create a record if there
    are no other users on the goal

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,71 @@
 class Habit < ActiveRecord::Base
   has_and_belongs_to_many :users
   has_many :checkins, order: "created_at DESC"
+
+  def self.associate_matching_or_create(habit_params, current_user)
+    associate_matching(habit_params, current_user) do |match|
+      if match
+        match
+      else
+        current_user.habits.create(habit_params)
+      end
+    end
+  end
+
+  def convert_or_update(habit_params, current_user)
+    if will_be_private(habit_params)
+      convert_to_private habit_params, current_user
+    elsif will_be_public(habit_params)
+      convert_to_public habit_params, current_user
+    else
+      update_attributes habit_params
+    end
+
+    self
+  end
+
+  private
+
+  def convert_to_public(habit_params, current_user)
+    associate_matching_or_update habit_params, current_user
+  end
+
+  def convert_to_private(habit_params, current_user)
+    private_habit = Habit.associate_matching_or_create(habit_params, current_user)
+    checkins.where(user_id: current_user.id).each do |checkin|
+      checkin.habit = private_habit
+      checkin.save!
+    end
+    users.destroy current_user
+  end
+
+  def public?
+    !private?
+  end
+
+  def will_be_private(habit_params)
+    public? and habit_params[:private] == true
+  end
+
+  def will_be_public(habit_params)
+    private? and habit_params[:private] == false
+  end
+
+  def associate_matching_or_update(habit_params, current_user)
+    Habit.associate_matching(habit_params, current_user) do |match|
+      if match
+        users.destroy current_user
+      else
+        update_attributes(habit_params)
+      end
+    end
+  end
+
+  def self.associate_matching(habit_params, current_user, &block)
+    matching_habit = Habit.where(private: habit_params[:private], title: habit_params[:title]).first
+    if matching_habit and !matching_habit.users.include?(current_user)
+      current_user.habits << matching_habit
+    end
+    yield(matching_habit)
+  end
 end
