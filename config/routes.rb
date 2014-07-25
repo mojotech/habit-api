c5362ba4a0850afec5d7632d6faaedec7121b831
ACRE <acre@mojotech.com>
2014-07-25 18:47:10 +0000

    Realign method arguments that wrap to the next line.
    2 files were modified.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Habitapp::Application.routes.draw do
   get 'home/index'
   root to: 'home#index'
   devise_for :users, controllers: {
-    sessions: 'sessions',
-    registrations: 'registrations'
-  }
+               sessions: 'sessions',
+               registrations: 'registrations'
+             }
   resources :habits
   resources :checkins
 end
