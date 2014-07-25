d4534562e0fa24c0aa747b8960b88f8ecece3d2b
ACRE <acre@mojotech.com>
2014-07-25 18:01:24 +0000

    Insert underscores into literal numbers.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140718155926) do
+ActiveRecord::Schema.define(version: 20_140_718_155_926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
