9a9880402186c6c2c9402e1c621b1a698513bd7e
Aaron Ellis Snyder <snyder@mojotech.com>
2014-07-29 12:33:58 -0400

    remove devise comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
+  include TokenAuthenticable
+
   has_and_belongs_to_many :habits
   has_many :checkins
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  include TokenAuthenticable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
