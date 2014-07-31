663cace425bf053d80730a900385ef5ea19bf596
Dylan Kirby <djkirby@wpi.edu>
2014-07-31 13:06:35 -0400

    test using the typeahead

--- features/widgets/type_ahead.rb
+++ features/widgets/type_ahead.rb
@@ -4,8 +4,20 @@ class TypeAhead < Dill::List
 
   def has_item?(title)
     items.each do |item|
-      return true if title == item.value[/^(.+?) \(\d+?\)$/, 1]
+      return true if title == title(item)
     end
     false
+  end
+
+  def click_suggestion(title)
+    if has_item?(title)
+      items.each do |item|
+        item.click if title == title(item)
+      end
+    end
+  end
+
+  def title(item)
+    item.value[/^(.+?) \(\d+?\)$/, 1]
   end
 end
