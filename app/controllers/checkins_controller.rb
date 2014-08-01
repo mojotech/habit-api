a2674fb72955c21dac95a18d64d6a530a55609a5
Aaron Ellis Snyder <snyder@mojotech.com>
2014-08-01 09:47:03 -0400

    correctly name checkin_params

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -1,18 +1,18 @@
 class CheckinsController < ApplicationController
   include TokenAuthentication
-  
+
   before_filter :authenticate_user_from_token!
   before_filter :authenticate_user!
 
   def create
     habit = current_user.habits.find(params[:checkin][:habit_id])
 
-    render json: habit.checkins.create(post_params.merge({ user_id: current_user.id }))
+    render json: habit.checkins.create(checkin_params.merge({ user_id: current_user.id }))
   end
 
   private
 
-  def post_params
+  def checkin_params
     params
       .require(:checkin)
       .permit(:value, :habit_id, :note)
