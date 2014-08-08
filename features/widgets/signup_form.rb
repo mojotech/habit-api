19467233cb576a68fce67fd7c5e7937cb2b492e4
Dylan Kirby <djkirby@wpi.edu>
2014-08-08 16:27:23 -0400

    ui form validation for signup

--- features/widgets/signup_form.rb
+++ features/widgets/signup_form.rb
@@ -1,12 +1,12 @@
 class SignupForm < Dill::FieldGroup
-  root 'form'
+  root '.signup-form'
 
   text_field :email, 'identification'
   text_field :password, 'password'
   widget :submit, 'button'
 
   widget :error, -> (message) {
-    ['.errors .error', text: message]
+    ['.alert .error', text: message]
   }
 
   def submit_form
