75dc3db5691360be0bf85b584276667a2cbf2c4b
Dylan Kirby <djkirby@wpi.edu>
2014-08-13 10:43:02 -0400

    Delete json_fialure_app.rb
    unneeded

--- lib/json_fialure_app.rb
+++
@@ -1,7 +0,0 @@
-class JsonFailureApp < Devise::FailureApp
-  def respond
-    self.status = 401
-    self.content_type = 'json'
-    self.response_body = '{"error" : "authentication error"}'
-  end
-end
