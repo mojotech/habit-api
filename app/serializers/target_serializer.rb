aadd9df4cf2e40dd091fd94764da3f32e5bca56b
Dylan Kirby <djkirby@wpi.edu>
2014-08-14 10:51:17 -0400

    add completion percentage to targets

--- app/serializers/target_serializer.rb
+++ app/serializers/target_serializer.rb
@@ -1,3 +1,3 @@
 class TargetSerializer < ActiveModel::Serializer
-  attributes :id, :value, :timeframe, :user_id, :habit_id, :unit
+  attributes :id, :value, :timeframe, :user_id, :habit_id, :unit, :completion
 end
