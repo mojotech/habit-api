28669b8013b53f8e72a1ebec877c3ce318ecb7b3
Aaron Ellis Snyder <snyder@mojotech.com>
2014-09-05 11:47:10 -0400

    use user_serializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,13 @@ class UsersController < ApplicationController
     if request.patch?
       current_user.display_name = params[:display_name]
       if current_user.save
-        render json: current_user.to_json, status: 200
+        render json: current_user, status: 200
       else
         render json: current_user.errors, status: 406
       end
     else
       if user_signed_in?
-        render json: current_user.to_json
+        render json: current_user
       else
         render json: { error: 'user not authenticated' }
       end
