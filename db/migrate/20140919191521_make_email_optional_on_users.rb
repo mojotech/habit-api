35eb7621dc63164b2884a27b44b73bac01f9207a
Aaron Ellis Snyder <snyder@mojotech.com>
2014-09-19 16:42:02 -0400

    make email optional if a provider exists

---
+++ db/migrate/20140919191521_make_email_optional_on_users.rb
@@ -0,0 +1,5 @@
+class MakeEmailOptionalOnUsers < ActiveRecord::Migration
+  def change
+    change_column :users, :email, :string, default: '', null: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905143609) do
+ActiveRecord::Schema.define(version: 20140919191521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20140905143609) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
