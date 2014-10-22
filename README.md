24cbdf9458732cff1e3d0fa272f25f9996c9874b
katrynmc <katryn@mojotech.com>
2014-10-22 16:33:31 -0400

    Password reset actually generates and sends an email. [fixes #133]

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Habit building or breaking through analytics.
 * Install postgres, on mac you're best bet is: http://postgresapp.com/
 * Ensure you have a habit role: ``createuser -s habit``
 * rake db:create db:setup
-* rails s
+* foreman start (host: localhost:5000)
 
 ## Tests
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,22 @@ Habitapp::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
+  # Raise an exception if there is an error when sending an email
   config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.default_url_options = { :host => 'localhost:5000' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
