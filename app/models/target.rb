63cfd4181a90c7354b8b7a3ebf01efa5288934e6
Aaron Ellis Snyder <snyder@mojotech.com>
2014-10-22 10:23:18 -0400

    require habits to exist before a target is created

--- app/models/target.rb
+++ app/models/target.rb
@@ -7,6 +7,7 @@ class Target < ActiveRecord::Base
   validates :value, numericality: { only_integer: true }
   validates :unit, :completion, presence: true
 
+  validates_presence_of :habit
   accepts_nested_attributes_for :habit, reject_if: :habit_exists
 
   def timeframe_int
